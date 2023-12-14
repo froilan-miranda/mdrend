20cef38113e4d7bb1196fa648be9d06adfb9e9d6
vaquero <froilan@virtual-artifact.com>
2023-12-13 19:20:15 -0500

    create initial files and add clap to project

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use clap::{clap_app, crate_version};
+
+fn main() {
+    let clap = clap_app!(mdrend =>
+        (version:crate_version!())
+        (author:"Froilan Miranda")
+        (about:"Renders markdown as you like")
+        (@arg input: + required "Sets the input file")
+    )
+    .get_matches();
+
+    println!("Input = {:?}", clap.value_of("input"));
+    println!("done")
+}
