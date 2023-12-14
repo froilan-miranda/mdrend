38ab38c5b881ebd4a22e8d8cec499b5d3e71fdd2
vaquero <froilan@virtual-artifact.com>
2023-12-14 00:10:51 -0500

    update clap to v4 and other packages using builder pattern

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-#![feature(proc_macro_hygiene)]
-use clap::{clap_app, crate_version};
+use clap::{Command, Arg, ArgAction};
 use pulldown_cmark::{html::push_html, Event, Parser};
 use maud::html;
 
-fn wrap_html(s:&str, css:Option<&str>)->String{
+fn wrap_html(s:&str, css:Option<&String>)->String{
     let res = html!{
         (maud::DOCTYPE)
             html {
@@ -22,6 +21,33 @@ fn wrap_html(s:&str, css:Option<&str>)->String{
 }
 
 fn main() {
+    let matches = Command::new("mdrend")
+        .version("0.1")
+        .author("Froilan Miranda")
+        .about("Renders markdown as you like")
+        .arg(Arg::new("input").help("Sets the input file").required(true))
+        .arg(Arg::new("wrap").help("Wrap in html").short('w').action(ArgAction::SetTrue))
+        .arg(Arg::new("event").help("Print events").short('e').action(ArgAction::SetTrue))
+        .arg(Arg::new("css").help("Link to css").long("css"))
+        .get_matches();
+
+    println!("Input = {:?}", matches.get_one::<String>("input"));
+    let infile = std::fs::read_to_string(matches.get_one::<String>("input").unwrap()).expect("Could not read file");
+
+    let mut res = String::new();
+    let ps = Parser::new(&infile);
+
+    let ps: Vec<Event> = ps.into_iter().collect();
+    if matches.get_flag("event"){
+        for p in &ps {
+            println!("{:?}", p);
+        }
+    }
+    push_html(&mut res, ps.into_iter());
+    if matches.get_flag("wrap"){
+        res = wrap_html(&res, matches.get_one::<String>("css"));
+    }
+    /*
     let clap = clap_app!(mdrend =>
         (version:crate_version!())
         (author:"Froilan Miranda")
@@ -35,7 +61,6 @@ fn main() {
 
     println!("Input = {:?}", clap.value_of("input"));
     let infile = std::fs::read_to_string(clap.value_of("input").unwrap()).expect("Could not read file");
-
     let mut res = String::new();
     let ps = Parser::new(&infile);
 
@@ -51,5 +76,6 @@ fn main() {
     if clap.is_present("wrap"){
         res = wrap_html(&res, clap.value_of("css"));
     }
+    */
     println!("{}", res);
 }
